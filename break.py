c5c1f8086739af3c8385ec0b829c98e36c2cd7d1
Priyankamahajan11 <82284404+Priyankamahajan11@users.noreply.github.com>
2021-05-03 13:07:20 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,22 @@
+"""n=int(input("enter any number"))
+for i in range(10):
+    if i==7:
+        print("helooo")
+        break
+    print(i)
+"""
+""""
+cart=[10,20,23,501,22,44,40]
+for item in cart:
+    if item>500 :
+        print("sorry we cant palce this order")
+        continue
+    print("processing item",item)
+
+"""
+number=[10,20,0,5,30,0]
+for i in number:
+    if i==0:
+        print("sorry we cant process this pagal ho gya kya tu")
+        continue
+    print("100/{}={}".format(i,100/i))
